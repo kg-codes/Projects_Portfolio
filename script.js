9c3734497929052a74b70c6668df3e8dfba42222
kg-codes <kirstiegonzales@gmail.com>
2023-03-03 08:59:10 -0800

    Portfolio V1

---
+++ script.js
@@ -0,0 +1,36 @@
+// var navscroll = document.querySelector('.navbar');
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 80) {
+//         navscroll.classList.add('navbar-scroll');
+//     } else if (window.scrollY <= 80) {
+//         navscroll.classList.remove('navbar-scroll');
+//     }
+// })
+
+function over(element) {
+    element.src = "./Images/me-nyc.jpg"
+}
+function out(element) {
+    element.src = "./Images/me-dj.jpg"
+}
+
+function bball(element) {
+    element.src = "./Images/white-sands.JPG"
+}
+function nyc(element) {
+    element.src = "./Images/me-antelope.jpeg"
+}
+
+function dj(element) {
+    element.src = "./Images/bball.jpeg"
+}
+function coding(element) {
+    element.src = "./Images/coding.jpg"
+}
+
+function bubba(element) {
+    element.src = "./Images/day-sup.jpg"
+}
+function bubba2(element) {
+    element.src = "./Images/night-sup.jpeg"
+}
